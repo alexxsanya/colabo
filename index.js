eab8cdaac497b9df20b598592e0222dba9a177f2
SMLukwiya <sundaymorganl@gmail.com>
2020-02-24 13:01:04 +0300

    Making first commit from branch1

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', (req, res) => res.send('Posting'))
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
